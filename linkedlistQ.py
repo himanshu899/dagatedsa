d21a30b44cd98f884ef97a69fee8617d01ec3e47
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-09-27 18:58:21 +0530

    Adding logic for deletion of node from  a linked list

--- linkedlistQ.py
+++ linkedlistQ.py
@@ -45,9 +45,13 @@ class LinkedList:
         curr = self.head
         while(curr != None):
             if curr.data == prev_value:
+                # attaching the remaining nodes after our prev_value to the new node, so that they don't get missed
                 new_node.next = curr.next
-                # temp = curr.next
+                # now assigning the next of current node to new_node, it will break connection between current node 
+                # and all nodes after that, But we've already assigned the remaining nodes to new node
                 curr.next = new_node
+                self.n +=1 
+                break
             curr = curr.next
         return new_node
     
@@ -69,6 +73,57 @@ class LinkedList:
 
         print(result[:-2])    
 
+    def delete_head(self):
+        if self.head != None:
+            self.head = self.head.next
+            self.n -=1
+            return self.head
+        else:
+            return "No node to delete"
+        
+    def pop(self):
+        curr = self.head
+        if curr == None:
+            return "Empty linked list"
+        if curr.next == None:
+            return self.delete_head()
+    
+        # Use this condition very carefully, as only using this won't work when linkedlist has single item
+        while(curr.next.next != None):
+            curr = curr.next
+        curr.next = None    
+        self.n -=1
+        return self.head
+    
+    def delete_specific_node(self, value):
+        curr = self.head
+        if curr == None:
+            return "Linked list is empty"
+        
+        if curr.data == value:
+            self.head = self.head.next
+            return self.head
+        prev = curr
+        while curr.next != None:
+            if curr.next.data == value:
+                print(" pehle ", curr.data)
+                curr.next = curr.next.next
+                print("baad me", curr.data)
+                break
+            # prev = curr    
+            curr = curr.next    
+
+        # while curr != None:
+        #     if curr.data == value:
+        #         print(" pehle ", prev.data)
+        #         prev.next = curr.next
+        #         print("baad me", prev.data)
+        #         break
+        #     prev = curr    
+        #     curr = curr.next    
+
+
+
     def search(self):
         return
 
@@ -88,7 +143,22 @@ l1.insert_tail(9)
 l1.insert_after(1, 8)
 
 # print(l1.head.data)
-# print(l1.len())
+print(l1.len())
+l1.traverse()
+
+l1.delete_specific_node(3)
+l1.traverse()
+
+l1.delete_specific_node(5)
+l1.traverse()
+
+l1.delete_specific_node(9)
+l1.traverse()
+
+l1.delete_specific_node(1)
+l1.traverse()
+
+l1.delete_specific_node(10)
 l1.traverse()
 # print(l1.search())
 
